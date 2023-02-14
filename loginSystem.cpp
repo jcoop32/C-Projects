b70306f9221d95f1d1a89bd12d834356d512922c
Josh Cooper <joshcooper@matrixexe.local>
2023-02-14 12:23:43 -0600

    login and registration update

--- loginSystem.cpp
+++ loginSystem.cpp
@@ -3,48 +3,146 @@
 
 using namespace std;
 
-
-void login(){
-
-    time_t now = time(0);
-    char * dt = ctime(& now);
-    cout << "Username: ";
+void pokemon()
+{
+    cout << "quu..__	" << endl;
+    cout << "$$$b  `---.__	" << endl;
+    cout << "$$b        `--.                          ___.---uuudP	" << endl;
+    cout << "   `$$b           `.__.------.__     __.---'      $$$$\"              .	" << endl;
+    cout << "     \"$b          -'            `-.-'            $$$\"              .'|	" << endl;
+    cout << "       \".                                       d$\"             _.'  |	" << endl;
+    cout << "         `.   /                              ...\"             .'     |	" << endl;
+    cout << "           `./                           ..::-'            _.'       |	" << endl;
+    cout << "            /                         .:::-'            .-'         .'	" << endl;
+    cout << "           :                          ::''\\          _.'            |	" << endl;
+    cout << "          .' .-.             .-.           `.      .'               |	" << endl;
+    cout << "          : /'$$|           .@\"$\\           `.   .'              _.-'	" << endl;
+    cout << "         .'|$u$$|          |$$,$$|           |  <            _.-'	" << endl;
+    cout << "         | `:$$:'          :$$$$$:           `.  `.       .-'	" << endl;
+    cout << "         :                  `\"--'             |    `-.     \\	" << endl;
+    cout << "        :##.       ==             .###.       `.      `.    `\\	" << endl;
+    cout << "        |##:                      :###:        |        >     >	" << endl;
+    cout << "        |#'     `..'`..'          `###'        x:      /     /		" << endl;
+    cout << "         \\                                   xXX|     /    ./	" << endl;
+    cout << "          \\                                xXXX'|    /   ./	" << endl;
+    cout << "          /`-.                                  `.  /   /	" << endl;
+    cout << "         :    `-  ...........,                   | /  .'	" << endl;
+    cout << "         |         ``:::::::'       .            |<    `.	" << endl;
+    cout << "         |             ```          |           x| \\ `.:``.	" << endl;
+    cout << "         |                         .'    /'   xXX|  `:`M`M':.	" << endl;
+    cout << "         |    |                    ;    /:' xXXX'|  -'MMMMM:'	" << endl;
+    cout << "         `.  .'                   :    /:'       |-'MMMM.-'	" << endl;
+    cout << "          |  |                   .'   /'        .'MMM.-'	" << endl;
+    cout << "          `'`'                   :  ,'          |MMM<	" << endl;
+    cout << "            |                     `'            |tbap\\	" << endl;
+    cout << "             \\                                  :MM.-'	" << endl;
+    cout << "              \\                 |              .''	" << endl;
+    cout << "               \\.               `.            /	" << endl;
+    cout << "                /     .:::::::.. :           /	" << endl;
+    cout << "               |     .:::::::::::`.         /	" << endl;
+    cout << "               |   .:::------------\\       /	" << endl;
+    cout << "              /   .''               >::'  /	" << endl;
+    cout << "              `',:                 :    .'	" << endl;
+}
+class UserInfo
+{
+public:
+    string firstName;
+    string lastName;
     string username;
-    cin >> username;
-    cout << "Enter password: ";
-    string password1;
-    cin >> password1;
-    cout << "Verify password: ";
-    string password2;
-    cin >> password2;
-    if (password2 == password1)
+    string email;
+    string initPassword;
+    string verifyPassword;
+};
+
+void reg()
+{
+    UserInfo reg;
+    cout << "Enter First Name: ";
+    cin >> reg.firstName;
+
+    cout << "Enter Last Name: ";
+    cin >> reg.lastName;
+
+    cout << "Enter username: ";
+    cin >> reg.username;
+
+    cout << "Enter email: ";
+    cin >> reg.email;
+
+    cout << "Enter Password: ";
+    cin >> reg.initPassword;
+
+    cout << "Verify Password: ";
+    cin >> reg.verifyPassword;
+
+    if (reg.initPassword == reg.verifyPassword)
     {
-        cout << "Welcome " << username << "!" << endl;
-        cout << "Do you want to Logout? (Y/N): ";
-        char userLOQ;
-        cin >> userLOQ;
-        if (userLOQ == 'y' || 'Y')
-        {
-            cout << "Log Out Success! " << dt << endl;
-            login();
-        }
+        cout << "REGISTRATION COMPLETE!" << endl;
+        cout << "Name: " << reg.firstName << " " << reg.lastName << endl;
+        cout << "Username: " << reg.username << endl;
+        cout << "Email: " << reg.email << endl;
+        cout << "Password: " << reg.initPassword << endl;
+        cout << "_________________________________" << endl;
     }
     else
     {
         cout << "error! Passwords do not match!" << endl;
-        login();
     }
 }
 
-int main()
+void login()
 {
-    cout << "Login ('L') or Register ('R'): ";
-    char userReponse;
-    cin >> userReponse;
-    while (userReponse == 'L' || 'l')
-    {
-        login();
-    }
 
-    return 0;
+    UserInfo log;
+    time_t now = time(0);
+    char *dt = ctime(&now);
+    cout << "Username: ";
+    cin >> log.username;
+
+    cout << "Enter password: ";
+    cin >> log.initPassword;
+
+    cout << "Verify password: ";
+    cin >> log.verifyPassword;
+
+    if (log.initPassword == log.verifyPassword)
+    {
+        cout << "Welcome, " << log.username << "!" << endl;
+        cout << "Do you want to Logout? (Y/N): ";
+        char userLOQ;
+        cin >> userLOQ;
+        if (userLOQ == 'Y')
+        {
+            cout << "Log Out Success! " << dt << endl;
+        }
+        else
+        {
+            pokemon();
+        }
+    }
+    else
+        {
+            cout << "error! Passwords do not match!" << endl;
+        }
 }
+
+    int main()
+    {
+        char userR;
+        cout << "Login ('L') or Register ('R'): ";
+        cin >> userR;
+        if (userR == 'R')
+        {
+            reg();
+            login();
+        }
+        else if (userR == 'L')
+        {
+            login();
+        }
+        else
+        {
+            cout << "Please enter valid character!" << endl;
+        }
+    }
